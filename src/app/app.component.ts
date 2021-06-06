3d50b0f9c8ea7f832d103645fc6c39850a77cb85
ThinkPad <tyrontundrom@gmail.com>
2021-06-06 14:46:39 +0200

    one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  public todos: string[] = [];
+
+  public todoForm = new FormGroup({
+    todo: new FormControl('', Validators.required)
+  });
+
+
+  onSubmit() {
+    console.log(this.todoForm.value.todo)
+    const newTodo = this.todoForm.value.todo
+    if (newTodo !== '') {
+      this.todos.push(newTodo);
+      this.todoForm.setValue({
+        todo: ''
+      });
+    }
+  }
 }
